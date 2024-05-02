9811499a7ae5e961369f36dba8b2a6e48a153730
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2024-05-02 10:21:25 +0100

    add extra route

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,16 @@ use App\Http\Controllers\V1\Tasks\GetAllTask;
 */
 Route::group(['prefix' => '/'], function () {
     Route::get('tasks', GetAllTask::class);
+    Route::put('tasks', GetAllTask::class)->name('tasks.update');
+    Route::put('delete', GetAllTask::class)->name('tasks.delete');
     Route::post('tasks', GetAllTask::class)->name('tasks.create');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Project Route
+|--------------------------------------------------------------------------
+*/
+Route::group(['prefix' => '/'], function () {
+    Route::post('projects', GetAllTask::class)->name('projects');
 });
