d1ac720be62c46fb92a20f0fabd815bedabdd806
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2024-05-02 09:32:07 +0100

    add controller

---
+++ app/Http/Controllers/V1/Tasks/GetAllTask.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\V1\Tasks;
+
+/*
+|--------------------------------------------------------------------------
+| Illuminate Namespace
+|--------------------------------------------------------------------------
+*/
+use App\Http\Controllers\Controller;
+
+class GetAllTask extends Controller
+{
+    use ResponseTrait;
+
+    public function __construct
+    (
+    ) {}
+
+    public function __invoke()
+    {
+
+    }
+}
